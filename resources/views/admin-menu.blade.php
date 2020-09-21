4a9bcbbff4089fcf0818fbd44ee8fbe72450f941
Haidar Alruwayjih <h.rowaijeh@hotmail.com>
2020-09-21 01:42:00 -0400

    Fixing footer

--- resources/views/admin-menu.blade.php
+++ resources/views/admin-menu.blade.php
@@ -1,20 +1,22 @@
 @extends('layouts.index')
 
 @section('content')
-<div class="container">
-	<h2 class="text-center mt-3">What do you want to do?</h2>
-	<div class="row">
-		<div class="col-12 text-center mt-3">
-			<a class="btn btn-color btn-menu mr-2" href="#" role="button" data-toggle="tooltip" data-placement="left" title="Star a fresh lead, Assing a seller, Assing a supervisor">Create Lead</a>
-			<a class="btn btn-color btn-menu" href="#" role="button" data-toggle="tooltip" data-placement="bottom" title="View Active Leads, Lost Leads, Closed Leads">View Leads</a>
-			<a class="btn btn-color btn-menu ml-2" href="#" role="button" data-toggle="tooltip" data-placement="right" title="Clients, Salesmen, Supervisors, Admins">Manage Users</a>
+<main>
+	<div class="container">
+		<h2 class="text-center mt-3">What do you want to do?</h2>
+		<div class="row">
+			<div class="col-12 text-center mt-3">
+				<a class="btn btn-color btn-menu mr-2" href="#" role="button" data-toggle="tooltip" data-placement="left" title="Star a fresh lead, Assing a seller, Assing a supervisor">Create Lead</a>
+				<a class="btn btn-color btn-menu" href="#" role="button" data-toggle="tooltip" data-placement="bottom" title="View Active Leads, Lost Leads, Closed Leads">View Leads</a>
+				<a class="btn btn-color btn-menu ml-2" href="#" role="button" data-toggle="tooltip" data-placement="right" title="Clients, Salesmen, Supervisors, Admins">Manage Users</a>
+			</div>
+			<div class="col-12 mt-3 text-center">
+				<a class="btn btn-color btn-menu mr-6" href="#" role="button" data-toggle="tooltip" data-placement="left" title="View Report, View Graphs, Generate Reports, Generate Graphs"> Stats</a>	
+				<a class="btn btn-color btn-menu ml-6" href="#" role="button" data-toggle="tooltip" data-placement="right" title="Sep Stages, Set Criterias">Septup System</a>
+			</div>
 		</div>
-		<div class="col-12 mt-3 text-center">
-			<a class="btn btn-color btn-menu mr-6" href="#" role="button" data-toggle="tooltip" data-placement="left" title="View Report, View Graphs, Generate Reports, Generate Graphs"> Stats</a>	
-			<a class="btn btn-color btn-menu ml-6" href="#" role="button" data-toggle="tooltip" data-placement="right" title="Sep Stages, Set Criterias">Septup System</a>
-		</div>
-	</div>
-</div><br><br><br><br>
+	</div><br><br><br><br>
+</main>
 @endsection
 
 @section('js-extra')
--- resources/views/supervisor-menu.blade.php
+++ resources/views/supervisor-menu.blade.php
@@ -1,14 +1,16 @@
 @extends('layouts.index')
 
 @section('content')
-<div class="container">
-	<h2 class="text-center mt-3">What do you want to do?</h2>
-	<div class="row mt-3">
-		<div class="col-12 text-center">
-			<a class="btn btn-color btn-menu" href="#" role="button" data-toggle="tooltip" data-placement="bottom" title="View Active Leads, Lost Leads, Closed Leads">View Leads</a>
+<main>
+	<div class="container">
+		<h2 class="text-center mt-3">What do you want to do?</h2>
+		<div class="row mt-3">
+			<div class="col-12 text-center">
+				<a class="btn btn-color btn-menu" href="#" role="button" data-toggle="tooltip" data-placement="bottom" title="View Active Leads, Lost Leads, Closed Leads">View Leads</a>
+			</div>
 		</div>
-	</div>
-</div><br><br><br><br>
+	</div><br><br><br><br>
+</main>
 @endsection
 
 @section('js-extra')
