3c18be07889dc41b5b654f3b2b6929fa2f0505c4
Haidar Alruwayjih <h.rowaijeh@hotmail.com>
2020-09-23 03:44:30 -0400

    view report creation

--- resources/views/stats.blade.php
+++ resources/views/stats.blade.php
@@ -31,7 +31,66 @@
 			  	<div class="container">
 				  	<div class="card mt-4">
 					  <div class="card-body">
-					    This is some text within a card body.
+					  	<form>
+						  <div class="form-row">
+						    <div class="col-6">
+						        <div class="form-group row">
+								    <label for="date-start" class="col-3 col-form-label text-right">Start date:</label>
+								    <div class="col-9">
+								      <input type="text" class="form-control" id="date-start" placeholder="dd/mm/yyyy">
+								    </div>
+								</div>
+						    </div>
+						    <div class="col-6">
+						      <div class="form-group row"> 
+								    <label for="date-end" class="col-3 col-form-label text-right" >End date:</label>
+								    <div class="col-9">
+								      <input type="text" class="form-control" id="date-end" placeholder="dd/mm/yyyy">
+								    </div>
+								</div>
+						    </div>
+						    <div class="col-12 mt-3">
+						    	<h6 class="mb-4">For this range there are:</h6>
+						    	<div class="row">
+							    	<div class="form-group col-4 text-center">
+									    <label for="number-lead">Total number of Leads</label>
+									    <input type="number" class="form-control text-center" id="number-lead">
+									</div>
+									<div class="form-group col-4 text-center">
+									    <label for="number-successful-lead">Successful leads</label>
+									    <input type="number" class="form-control text-center" id="number-successful-leads">
+									</div>
+									<div class="form-group col-4 text-center">
+									    <label for="number-lost-lead">Lost leads</label>
+									    <input type="number" class="form-control text-center" id="number-lost-lead">
+									</div>		
+						    	</div>
+						    	<h6 class="mb-4">That come from:</h6>
+						    	<div class="row">
+						    		<div class="form-group col-4 text-center">
+									    <label for="number-facebook">Facebook</label>
+									    <input type="number" class="form-control text-center" id="number-facebook">
+									</div>
+									<div class="form-group col-4 text-center">
+									    <label for="number-instagram">Instagram</label>
+									    <input type="number" class="form-control text-center" id="number-instagram">
+									</div>
+									<div class="form-group col-4 text-center">
+									    <label for="number-ads">Google Ads</label>
+									    <input type="number" class="form-control text-center" id="number-ads">
+									</div>	
+						    	</div>	
+						    </div>
+						  </div>
+						   <div class="mt-4 d-flex justify-content-end">
+						        <div class="form-group row"> 
+								    <label for="total-sales" class="col-6 col-form-label text-right" ><b>Total Sales Profit: </b></label>
+								    <div class="col-6">
+								      <input type="number" class="form-control" id="total-sales" >
+								    </div>
+								</div>
+						   </div>
+						</form>		
 					  </div>
 					</div>		  		
 			  	</div>
